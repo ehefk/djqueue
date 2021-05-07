e45d5ff69bd772436da13e5a26955ee3f83d5cf2
Eh <69839118+ehefk@users.noreply.github.com>
2021-05-06 21:46:33 -0700

    finished logic for queue
    - finished logic twitch-discord DJ queue
    - just dance tracks IDs are in the database
    TODO:
    youtube link cleanups

--- djqueue.py
+++ djqueue.py
@@ -3,6 +3,22 @@ import asyncio
 import threading
 import discord_bot.discord_bot as discord_bot
 from twitchbot import BaseBot
+import sqlite3
+
+
+
+#############
+## clear requests db - will move this later
+##
+def botinit():
+    con = sqlite3.connect("database.sqlite")
+    cursor = con.cursor()
+    sqlite_query = """DELETE FROM song_requests;"""
+    cursor.execute(sqlite_query)
+    sqlite_query = """INSERT INTO song_requests (id, q_length) VALUES (9999, 0)"""
+    cursor.execute(sqlite_query)
+    con.commit()
+    con.close()
 
 
 def DiscordBot():
@@ -11,7 +27,7 @@ def DiscordBot():
     asyncio.set_event_loop(discordloop)
     asyncio.get_event_loop()
     bot = discord_bot.Bot()
-    discordloop.create_task(bot.start("ODIyMzE3NzU5MzIzOTYzNDIz.YFQhFw.eaX6iFiCkic_cNiBSikvmLj7l5U"))
+    discordloop.create_task(bot.start("ODIyMzE3NzU5MzIzOTYzNDIz.YFQhFw.GE7_YxFB0YjxYxThhB_KkFNbih0"))
     print("Starting Discord Loop")
     discordloop.run_forever()
 
@@ -27,6 +43,7 @@ def TwitchBot():
 
 
 if __name__ == '__main__':
+    botinit()
     # Create Communication Queues
     DiscordThread = threading.Thread(target=DiscordBot, args=())
     DiscordThread.daemon = False
